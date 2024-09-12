0a1f95007bd5dcd7d0c3ff5b7565aa2b0054b738
MaNo <mahmoudfalous@gmail.com>
2024-09-12 02:50:18 -0700

    tst

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { Button, Input, QRCode } from "antd";
+import { useState } from "react";
+import { DoubleRightOutlined } from "@ant-design/icons";
 
-function App() {
+export default function App() {
+  const [text, setText] = useState("");
+  const [qrCode, setQrCode] = useState("");
+  const [loading, setLoading] = useState("active");
+
+  function handelConvert() {
+    {
+      text !== "" &&
+        setInterval(() => {
+          setLoading("active");
+        }, 6000);
+    }
+    {
+      text !== "" && setQrCode(text);
+    }
+    // setLoading(true);
+    // setQrCode(text);
+
+    {
+      text !== "" && setLoading("loading");
+    }
+    setText("");
+    // setLoading("loading");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Qr qrCode={qrCode} text={text} load={loading} />
+      <InputValue text={text} onIn={setText} />
+      <ConvertBtn text={text} onClicking={handelConvert} />
     </div>
   );
 }
-
-export default App;
+function Qr({ qrCode, text, load }) {
+  return (
+    <>
+      <QRCode className="qr" status={load} value={qrCode || "-"} />
+    </>
+  );
+}
+function InputValue({ text, onIn }) {
+  return (
+    <Input
+      placeholder="Enter Text or link"
+      allowClear
+      maxLength={60}
+      showCount
+      value={text}
+      status={text === "" ? "error" : ""}
+      onChange={(e) => onIn(e.target.value)}
+      prefix={<DoubleRightOutlined />}
+    ></Input>
+  );
+}
+function ConvertBtn({ onClicking }) {
+  return (
+    <Button className="Btn" onClick={onClicking}>
+      Convert To Qr
+    </Button>
+  );
+}
