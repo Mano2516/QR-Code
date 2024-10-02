d8ba2e3998677fcfdff0b657857c6d3249e4e21e
MaNo <mahmoudfalous@gmail.com>
2024-10-01 19:24:43 -0700

    download

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./index.css";
-import { Button, Input, QRCode } from "antd";
+import { Button, Input, QRCode, Space } from "antd";
 import { useState } from "react";
 import { DoubleRightOutlined } from "@ant-design/icons";
 
@@ -7,39 +7,72 @@ export default function App() {
   const [text, setText] = useState("");
   const [qrCode, setQrCode] = useState("");
   const [loading, setLoading] = useState("active");
+  const [showDownload, setShowDownload] = useState(false);
 
   function handelConvert() {
-    {
-      text !== "" &&
-        setInterval(() => {
-          setLoading("active");
-        }, 6000);
-    }
+    setShowDownload(true);
+
     {
       text !== "" && setQrCode(text);
     }
     // setLoading(true);
     // setQrCode(text);
 
-    {
-      text !== "" && setLoading("loading");
-    }
     setText("");
+
     // setLoading("loading");
   }
 
+  function doDownload(url, fileName) {
+    const a = document.createElement("a");
+    a.download = fileName;
+    a.href = url;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+  const downloadCanvasQRCode = () => {
+    const canvas = document.getElementById("myqrcode")?.querySelector("canvas");
+    if (canvas) {
+      const url = canvas.toDataURL();
+      doDownload(url, "QRCode.png");
+    }
+    setQrCode("");
+    setShowDownload(false);
+  };
   return (
     <div className="App">
-      <Qr qrCode={qrCode} text={text} load={loading} />
+      <div
+        style={{
+          height: 200,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {showDownload && <Qr qrCode={qrCode} text={text} load={loading} />}
+      </div>
       <InputValue text={text} onIn={setText} />
-      <ConvertBtn text={text} onClicking={handelConvert} />
+      <Space direction="vertical" size={15}>
+        <ConvertBtn text={text} onClicking={handelConvert} />
+        {showDownload && (
+          <Button type="primary" onClick={downloadCanvasQRCode}>
+            Download
+          </Button>
+        )}
+      </Space>
     </div>
   );
 }
 function Qr({ qrCode, text, load }) {
   return (
     <>
-      <QRCode className="qr" status={load} value={qrCode || "-"} />
+      <QRCode
+        className="qr"
+        status={load}
+        value={qrCode || "-"}
+        id="myqrcode"
+      />
     </>
   );
 }
